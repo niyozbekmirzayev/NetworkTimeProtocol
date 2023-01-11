ccbab4f64d1990b460a35719cc009607494c4867
Niyozbek <n.mirzayev@baik.uz>
2023-01-11 09:33:11 +0500

    refacoring

--- NTPClient/Program.cs
+++ NTPClient/Program.cs
@@ -1,5 +1,4 @@
 ﻿
-using NTPClient;
 using System;
 using System.Net.Http;
 using System.Runtime.InteropServices;
@@ -13,15 +12,15 @@ namespace NtpClient
         {
             using var client = new HttpClient();
 
-            var response = await client.GetAsync($"http://localhost:5000/NTP?clientSentTime={DateTime.Now.ToString()}");
+            var clientSentDate = DateTime.Now;
+            var response = await client.GetAsync($"http://localhost:5000/NTP");
             var content = await response.Content.ReadAsStringAsync();
 
-            var ntpData = Newtonsoft.Json.JsonConvert.DeserializeObject<NTPResponse>(content);
+            var serverTime = Newtonsoft.Json.JsonConvert.DeserializeObject<DateTime>(content);
 
             // calculate delay and offset
             var clientReceivedTime = DateTime.Now;
-            var delay = (clientReceivedTime - ntpData.ClientSentTime) - (ntpData.ServerSentTime - ntpData.ServerReceivedTime);
-            var offset = ((ntpData.ServerReceivedTime - ntpData.ClientSentTime) + (ntpData.ServerSentTime - clientReceivedTime)) / 2;
+            var offset = ((serverTime - clientSentDate) + (serverTime - clientReceivedTime)) / 2;
 
             // set local time using offset 
             var localTime = clientReceivedTime + offset;
@@ -34,8 +33,7 @@ namespace NtpClient
         static void SetLocalTime(DateTime newTime)
         {
             // create a new SYSTEMTIME struct
-            SYSTEMTIME systemTime = new SYSTEMTIME();
-            // fill the struct
+            var systemTime = new SYSTEMTIME();
             systemTime.wYear = (ushort)newTime.Year;
             systemTime.wMonth = (ushort)newTime.Month;
             systemTime.wDayOfWeek = (ushort)newTime.DayOfWeek;
@@ -45,15 +43,12 @@ namespace NtpClient
             systemTime.wSecond = (ushort)newTime.Second;
             systemTime.wMilliseconds = (ushort)newTime.Millisecond;
 
-            // call the SetLocalTime function using the SYSTEMTIME struct
             if (!SetLocalTime(ref systemTime))
             {
-                // handle error
                 Console.WriteLine("Error setting the local time.");
             }
         }
 
-        // this struct is used for the SetLocalTime function
         [StructLayout(LayoutKind.Sequential)]
         private struct SYSTEMTIME
         {
@@ -66,6 +61,7 @@ namespace NtpClient
             public ushort wSecond;
             public ushort wMilliseconds;
         }
+        
         // this method is used to change the local time
         [DllImport("kernel32.dll")]
         private static extern bool SetLocalTime(ref SYSTEMTIME systemTime);
